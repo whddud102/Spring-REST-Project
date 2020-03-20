4217149ee22d7292b2cfbe7add86492e5005196b
Jongyoung <whddud@naver.com>
2020-03-20 16:22:30 +0900

    영속 영역의 CRUD 구현 및 테스트 완료

--- src/main/java/org/zerock/mapper/ReplyMapper.java
+++ src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -1,5 +1,24 @@
 package org.zerock.mapper;
 
-public interface ReplyMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
+
+public interface ReplyMapper {
+	
+	// 댓글 객체를 전달받아 DB에 등록
+	public int insert(ReplyVO vo);
+	
+	// 게시글 번호를 전달받고 해당하는 댓글을 보여줌
+	public ReplyVO read(Long rno);
+	
+	public int delete(Long rno);
+	
+	public int update(ReplyVO reply);
+	
+	public List<ReplyVO> getListWithPaging(
+			@Param("cri") Criteria cri,	// 페이징 처리 정보가 들어 있는 객체
+			@Param("bno") Long bno);	// 게시글을 특정할 게시글 번호
 }
--- src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -1,10 +1,17 @@
 package org.zerock.mapper;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -13,6 +20,10 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class ReplyMapperTests {
+
+	//테스트를 위한 게시글들의 bno 배열
+	private Long[] bnoArr = {20525L, 20524L, 20523L, 20521L, 20520L};
+	
 	
 	@Setter(onMethod_ = {@Autowired})
 	private ReplyMapper mapper;
@@ -21,6 +32,62 @@ public class ReplyMapperTests {
 	public void testMapper() {
 		log.info("\n\n------------------ Mapper의 의존성 주입 테스트 ---------------------");
 		log.info(mapper + "\n");
+	}
+	
+	@Test
+	public void testCreate() {
+		IntStream.range(1, 10).forEach(i -> {
+			ReplyVO vo = new ReplyVO();
+			
+			vo.setBno(bnoArr[i % 5]);
+			vo.setReply("댓글 테스트 " + i);
+			vo.setReply("replyer" + i);
+			
+			mapper.insert(vo);
+		});
+	}
+	
+	@Test
+	public void testRead() {
+		Long targetLno = 5L;
+		
+		ReplyVO vo = mapper.read(targetLno);
+		
+		log.info(vo);
+	}
+	
+	@Test
+	public void testDelete() {
+		Long targetLno = 1L;
+		
+		mapper.delete(targetLno);
+	}
+	
+	@Test
+	public void testUpdate() {
+		Long targetLno = 10L;
+		
+		ReplyVO vo = mapper.read(targetLno);
+		
+		vo.setReply("Update Reply");
+		
+		int count = mapper.update(vo);
+		
+		log.info("UPDATE COUNT: " + count);
+	}
+	
+	@Test
+	public void testList() {
+		
+		// 기본 검색 조건을 가진 객체를 생성
+		Criteria cri = new Criteria();
+		
+		// 목표 게시글 번호 = bnoArr[0] = 20525L
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
+		
+		replies.forEach(reply -> {
+			log.info(reply);
+		});
 	}
 	
 }
